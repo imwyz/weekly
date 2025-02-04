f96a96c30439586aac426a052ef6a1ceb0ffdbf2
ricky <kwkk@163.com>
2025-02-04 15:43:45 +0800

    修改图片

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "彦祖",
   description: "记录工程师 王彦祖 的不枯燥生活，每周一发布，欢迎关注",
   keywords: "YanZu,Weekly,Blog,前端,macOS,MiaoYan,Pake,Swift,设计",
-  icon: "https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png",
+  icon: "http://qn.wyz.im/wyz.jpeg",
   pic: "https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png",
   homePage: "https://wyz.im",
   blogPage: "https://www.wyz.im/",
